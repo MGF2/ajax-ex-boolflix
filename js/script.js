4b8a839763aecd3a897319829be028f744e9497b
Unknown <fioremartina@hotmail.com>
2020-09-02 12:55:30 +0200

    functioning API

--- js/script.js
+++ js/script.js
@@ -5,8 +5,55 @@ $(document).ready(function(){
     var input = $('#input').val();
     console.log(input);
 
-    input = $('#input').val('');
+    //inizio ajax
+    $.ajax(
+      {
+        url: 'https://api.themoviedb.org/3/search/movie',
+        method: 'GET',
+        data: {
+          api_key: 'a37d25ddedf8f63da854e0a07d06f5f1',
+          query: input,
+          language: 'it-IT',
+        },
+        success: function(risposta){
 
+          var movies = risposta.results;
+
+          // loop to get objects
+          for ( var i = 0 ; i < movies.length; i++) {
+
+            console.log(movies);
+
+            var source = $('#movie-template').html();
+            var template = Handlebars.compile(source);
+
+            // var context = {
+            //   title: movies[i].title,
+            //   original_title: movies[i].original_title,
+            //   original_language: movies[i].original_language,
+            //   vote_average: movies[i].vote_average,
+            // }
+
+            // save object content
+            var context = movies[i];
+            var html = template(context);
+
+            console.log(context);
+
+            //inserimento object in html
+            $('.container').append(html);
+
+            input = $('#input').val('');
+
+          }
+
+        },
+        'error':function(){
+          alert('errore!');
+        }
+      }
+    );
+     //fine ajax
   });
 
 
