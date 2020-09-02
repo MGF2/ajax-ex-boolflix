86a4805fcc07a75214bcd33d9d14bfb51fe88046
Unknown <fioremartina@hotmail.com>
2020-09-02 11:18:31 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('#search').click(function() {
+
+    var input = $('#input').val();
+    console.log(input);
+
+    input = $('#input').val('');
+
+  });
+
+
+});
